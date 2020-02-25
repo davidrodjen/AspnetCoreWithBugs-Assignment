6f6cc82e3bb1d61f557060b835315b634fef7ba6
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-02-25 12:48:07 -0800

    Create established in the ProductDB

--- AspnetCoreWithBugs/Data/ProductDb.cs
+++ AspnetCoreWithBugs/Data/ProductDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspnetCoreWithBugs.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +8,21 @@ namespace AspnetCoreWithBugs.Data
 {
     public static class ProductDb
     {
+        //public static async Task<Product> GetProductById(int id, ProductContext context)
+        //{
+        //    Product p = await (from product in context.Product
+        //                       where product.ProductId)
+        //}
 
-        ///public static async Task<Clothing> GetProductById(int id, )
+        public static async Task<Product> Create(Product p, ProductContext context)
+        {
+            await context.AddAsync(p);
+            await context.SaveChangesAsync();
+
+            return p;
+        }
+
+
 
 
 
